e5785e3cb8dfc2d32017d6d3f8ba4b92fa0d4df0
Will Brown <will@rebeagle.com>
2019-02-06 15:41:23 +0000

    working install script

--- src/OAKProxy/OAKProxy.psm1
+++ src/OAKProxy/OAKProxy.psm1
@@ -1,14 +1,34 @@
 function Install-OAKProxy {
     param (
-        # Create Service
-        # Create Event Log Source
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNull()]
+        [PSCredential]$Credential     
     )
+
+    New-EventLog -LogName "Application" -Source "OAKProxy"
+
     
+    if ($Credential.Password.Length) {
+        $args = @( "password=", $Credential.GetNetworkCredential().Password )
+    } else {
+        $args = @{}
+    }
+
+    # sc used for compatbility with gMSA. New-Service fails without a password.
+    sc.exe create "OAKProxy" start= "auto" depend= "NetLogon" DisplayName= "OAKProxy Server" `
+        binPath= "$PSScriptRoot\OAKProxy.exe -service" obj= $Credential.UserName @args
+    Start-Service -Name "OAKProxy"
+
+    "Installed OAKProxy service."
 }
 
 function Uninstall-OAKProxy {
     param (
-        
     )
-    
+
+    Stop-Service -Name "OAKProxy" -Force
+    sc.exe delete "OAKProxy"
+    Remove-EventLog -Source "OAKProxy"
+
+    "Uninstalled OAKProxy service."
 }
